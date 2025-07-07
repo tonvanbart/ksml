14d5de0a1549a8de58c5b965c894111523c1c247
Ton van Bart <ton@axual.com>
2025-07-07 13:22:48 +0200

    fix file names

--- scripts/extract_snippet.py
+++ scripts/extract_snippet.py
@@ -9,5 +9,5 @@ with open('ksml/src/test/resources/pipelines/test-aggregate-inline.yaml') as f:
 # Trim or extract as needed
 partial = {"pipelines": data.get("pipelines")}
 
-with open('docs/_snippets/example.yaml', 'w') as out:
+with open('docs/_snippets/test-aggregate-inline.yaml', 'w') as out:
     yaml.dump(partial, out, sort_keys=False)
