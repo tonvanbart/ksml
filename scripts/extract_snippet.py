832358ac38392edfd380c8db5244894bae96ffa6
Ton van Bart <tonvanbart@gmail.com>
2025-07-08 22:23:09 +0200

    copy all snippets

--- scripts/extract_snippet.py
+++ scripts/extract_snippet.py
@@ -3,11 +3,24 @@ import os
 
 os.makedirs('docs/_snippets', exist_ok=True)
 
-with open('ksml/src/test/resources/pipelines/test-aggregate-inline.yaml') as f:
-    data = yaml.safe_load(f)
+source_dir = os.Path('ksml/src/test/resources/pipelines')
+target_dir = os.Path('docs/_snippets')
+
+pipelines = list(source_dir.glob("*.yaml"))
+for definition in pipelines:
+    with open(definition, 'r') as f:
+        data = yaml.safe_load(f)
+        partial = {"pipelines": data.get("pipelines")}
+        output_path = target_dir / definition.name
+        with open(output_path, 'w') as out_file:
+            yaml.dump(partial, out_file, sort_keys=False)
+            print(f"extracted {definition} to {output_path}")
+
+# with open('ksml/src/test/resources/pipelines/test-aggregate-inline.yaml') as f:
+#     data = yaml.safe_load(f)
 
 # Trim or extract as needed
-partial = {"pipelines": data.get("pipelines")}
-
-with open('docs/_snippets/example.yaml', 'w') as out:
-    yaml.dump(partial, out, sort_keys=False)
+# partial = {"pipelines": data.get("pipelines")}
+#
+# with open('docs/_snippets/example.yaml', 'w') as out:
+#     yaml.dump(partial, out, sort_keys=False)
